b1b1e784a6c1ffc7f6b981f4e9d0b6f0af042055
huyhoangk5 <phuonglekjh@gmail.com>
2026-04-29 22:56:27 +0700

    Fix base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/ChefBot/',  // <-- QUAN TRỌNG: thay ChefBot bằng tên repo của bạn
   plugins: [
     react(),
     VitePWA({
